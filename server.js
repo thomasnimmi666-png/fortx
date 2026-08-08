7c84b11caf7bcd51cce1762e6db206ca747363f5
thomasnimmi666-png <thomasnimmi666@gmail.com>
2026-08-08 19:47:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+res.writeHead(200, {
+"Content-Type": "text/plain; charset=utf-8"
+});
+
+res.end("Privater Messenger Server läuft! 🔐");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+console.log(`Server läuft auf Port ${PORT}`);
+});
